8f72115fd1c8ac2994d59b5d64dd46bdec490753
Rodrigo Vaz <rodrigovaz@outlook.com>
2025-07-06 16:41:46 +0100

    casa

--- ft_printf.c
+++ ft_printf.c
@@ -11,8 +11,26 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
-int ft_printf(const char *, ...)
+int	ft_printf(const char *format, ...) // The function signature indicates that it takes a format string and a variable number of arguments, which is the three dots.
 {
-		int i;
+
+	va_list ap;
+	int count;
+
+	va_start(ap, format); // Initializes the variable argument list with the last named parameter (format).
+	count = 0; // Initialize the count of characters written to zero.
+	while (*format != '\0') // Loop through each character in the format string until the null terminator is reached.
+	{
+		if (*format == '%') // If the current character is a format specifier.
+
+	)
+int main()
+{
+	int count;
+
+	count = ft_printf("Hello %s/n", "Rodrigo");
+	ft_printf("The chars written are %d\n", count);
 }
